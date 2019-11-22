619d24ee4cf003ad7c6ce805ba2ef2d477aa0301
Chris Myers <cmyers2015@outlook.com>
2019-11-22 18:56:31 +0000

    Move WeatherForecast data fetch code to component base class

---
+++ BlazorClientTest/Client/Pages/WeatherForecastPageBase.cs
@@ -0,0 +1,20 @@
+﻿using BlazorClientTest.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorClientTest.Client.Pages
+{
+    public class WeatherForecastPageBase : ComponentBase
+    {
+        protected WeatherForecast[] forecasts;
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetJsonAsync<WeatherForecast[]>("api/WeatherForecast");
+        }
+    }
+}
