de2aebbc14df0da7d30f7f65064139f0c63c8180
Chris Myers <cmyers2015@outlook.com>
2020-01-07 20:22:47 +0000

    Added initial authentication base code. Currently authorised by default, will require plugging into webapi server

---
+++ BlazorClientTest/Client/Auth/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazorClientTest.Client.Auth
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            bool authenticated = true;
+            ClaimsIdentity identity;
+
+            if (authenticated)
+            {
+                identity = new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "TestUser")
+
+                }, "WebApiAuth");
+            }
+            else
+            {
+                identity = new ClaimsIdentity();
+            }
+
+            var user = new ClaimsPrincipal(identity);
+
+            return Task.FromResult(new AuthenticationState(user));
+        }
+
+    }
+}
